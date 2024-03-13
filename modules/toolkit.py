b8a8ef7b869e1aa2236a61a96f27a0d5770d4d5b
Tony-TRT <146631446+Tony-TRT@users.noreply.github.com>
2024-03-13 09:46:05 +0100

    Add documentation to toolkit.py module.

--- modules/toolkit.py
+++ modules/toolkit.py
@@ -9,6 +9,16 @@ from math import sqrt
 
 
 def load_images(folder: Path, alpha: bool = False) -> dict:
+    """Load images from a specified folder.
+
+    Args:
+        folder (Path): The path to the folder containing the images.
+        alpha (bool, optional): Whether to load images with alpha transparency. Defaults to False.
+
+    Returns:
+        dict: A dictionary where keys are the filenames (without extension) of the images
+        and values are the corresponding pygame.Surface objects representing the loaded images.
+    """
 
     return {
         image.stem: pygame.image.load(image).convert_alpha() if alpha else pygame.image.load(image).convert()
@@ -17,6 +27,19 @@ def load_images(folder: Path, alpha: bool = False) -> dict:
 
 
 def calculate_movement(destination: tuple[int, int], rect: Rect, dead_zone: int, velocity: int) -> tuple[float, float]:
+    """Calculate the movement required to reach a destination point.
+
+    Args:
+        destination (tuple[int, int]): The target coordinates (x, y) to move towards.
+        rect (Rect): The rectangle representing the current position and size of the object.
+        dead_zone (int): The radius within which no movement is necessary.
+        velocity (int): The speed at which the object should move.
+
+    Returns:
+        tuple[float, float]: A tuple containing the horizontal and vertical movement
+        required to reach the destination point. If the distance to the destination is within
+        the dead zone, returns (0, 0).
+    """
 
     # Calculate the horizontal and vertical differences
     # between the destination position and the center of the rectangle.
